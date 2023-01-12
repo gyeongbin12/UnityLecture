bce19339df6be21626b71f3daef9be1bce49be32
sesgoodboy <101865866+kang1492@users.noreply.github.com>
2023-01-12 21:49:04 +0900

    브로드 캐스트

---
+++ My project/Assets/First class/Script/Test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Monster
+{
+    public Monster()
+    {
+        Debug.Log("Monster가 태어났습니다.");
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    public int value = 100;
+    void Start()
+    {
+        Monster monster = new Monster();
+    }
+
+    void Update()
+    {
+        
+    }
+}
